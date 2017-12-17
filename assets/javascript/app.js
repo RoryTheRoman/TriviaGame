82856a1cc4782302b5d5f780eb996b8e11b9064e
Mike D <sprinkles@Mikes-MacBook-Pro.local>
2017-12-17 16:17:07 -0600

    Question/Answer Array aer Array added

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+var questions = [
+	["In what year was the original movie Clue first released in theaters?", "1985", "1982", "1988", "1994"],
+	["Comedy great Madeline Kahn played the role of what character?", "Ms. White", "Miss Scarlet", "The Cook", "Mr. Boddy"],
+	["Clue is one of the most well-scripted movies of all time", "True!", "False"],
+	["How many different endings does Clue have?", "3", "1", "2"],
+	["When the Cop answers the phone in the library, who is on the phone?", "J. Edgar Hooover", "Eleanor Roosevelt", "Michael Jackson", "A Salesman"],
+	["What was Ms. White's first husband's profession?", "Illusionist", "Doctor", "Tax Collector", "Politician"],
+	["How does Yvette die?  Was she:", "Strangled?", "Shot?", "Stabbed?",]
+	["What actress played the role of Mrs. Peacock?", "Eileen Brennan", "Faye Dunaway", "Carol Burnett","Shelley Duvall"],
+	["Miss Scarlet described <em>what</em> as a 'Red Herring?'", "Communism", "Fascism", "Dinner", "Christianity"],
+	["In the final version of the ending, who killed Mr. Boddy?", "Mr. Green", "Miss Scarlet", "Col. Mustard", "Mrs. Peacock"],
+	["Where and how was Mr. Boddy killed?", "In the Hall, with the Revolver", "In the Study, with the Candlestick", "In the Kitchen, with the Knife"],
+]
+
+var rightAnswers = 0;
+var wrongAnswers = 0;
+var nonAnswers = 0;
