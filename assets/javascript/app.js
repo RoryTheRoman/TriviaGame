139c48bcaf12c7826a1c272ba837dbb346c483a9
Mike D <sprinkles@Mikes-MacBook-Pro.local>
2017-12-23 08:26:50 -0600

    centered intro div html, styled a bit, hid mainContent div in jq

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,37 +11,50 @@
 // 	qTen : ["In the final version of the ending, who killed Mr. Boddy?", "Mr. Green", "Miss Scarlet", "Col. Mustard", "Mrs. Peacock"]
 // 	qEleven : ["Where and how was Mr. Boddy killed?", "In the Hall, with the Revolver", "In the Study, with the Candlestick", "In the Kitchen, with the Knife"]
 // };
+$(document).ready(function(){
+	$(".mainContent").hide();
 
-var quetions = [
-	"In what year was the original movie Clue first released in theaters?",
-	"Comedy great Madeline Kahn played the role of what character?",
-	"Clue is one of the most well-scripted movies of all time",
-	"How many different endings does Clue have?",
-	"When the Cop answers the phone in the library, who is on the phone?",
-	"What was Ms. White's first husband's profession?",
-	"How does Yvette die?  Was she:",
-	"What actress played the role of Mrs. Peacock?",
-	"Miss Scarlet described <em>what</em> as a 'Red Herring?'",
-	"In the final version of the ending, who killed Mr. Boddy?",
-	"Where and how was Mr. Boddy killed?",
-	]
-var answers = [
-	["1985", "1982", "1988", "1994"],
-	["Ms. White", "Miss Scarlet", "The Cook", "Mr. Boddy"],
-	["True!", "False"],
-	["3", "1", "2"],
-	["J. Edgar Hooover", "Eleanor Roosevelt", "Michael Jackson", "A Salesman"],
-	["Illusionist", "Doctor", "Tax Collector", "Politician"],
-	["Strangled?", "Shot?", "Stabbed?"],
-	["Eileen Brennan", "Faye Dunaway", "Carol Burnett","Shelley Duvall"],
-	["Communism", "Fascism", "Dinner", "Christianity"],
-	["Mr. Green", "Miss Scarlet", "Col. Mustard", "Mrs. Peacock"],
-	["In the Hall, with the Revolver", "In the Study, with the Candlestick", "In the Kitchen, with the Knife"],
-	]
+	var questions = [
+		"In what year was the original movie Clue first released in theaters?",
+		"Comedy great Madeline Kahn played the role of what character?",
+		"Clue is one of the most well-scripted movies of all time",
+		"How many different endings does Clue have?",
+		"When the Cop answers the phone in the library, who is on the phone?",
+		"What was Ms. White's first husband's profession?",
+		"How does Yvette die?  Was she:",
+		"What actress played the role of Mrs. Peacock?",
+		"Miss Scarlet described <em>what</em> as a 'Red Herring?'",
+		"In the final version of the ending, who killed Mr. Boddy?",
+		"Where and how was Mr. Boddy killed?",
+		]
+	var answers = [
+		["1985", "1982", "1988", "1994"],
+		["Ms. White", "Miss Scarlet", "The Cook", "Mr. Boddy"],
+		["True!", "False"],
+		["3", "1", "2"],
+		["J. Edgar Hooover", "Eleanor Roosevelt", "Michael Jackson", "A Salesman"],
+		["Illusionist", "Doctor", "Tax Collector", "Politician"],
+		["Strangled?", "Shot?", "Stabbed?"],
+		["Eileen Brennan", "Faye Dunaway", "Carol Burnett","Shelley Duvall"],
+		["Communism", "Fascism", "Dinner", "Christianity"],
+		["Mr. Green", "Miss Scarlet", "Col. Mustard", "Mrs. Peacock"],
+		["In the Hall, with the Revolver", "In the Study, with the Candlestick", "In the Kitchen, with the Knife"],
+		]
 
 
-console.log(answers);
-var rightAnswers = 0;
-var wrongAnswers = 0;
-var nonAnswers = 0;
+	console.log(answers);
+	var rightAnswers = 0;
+	var wrongAnswers = 0;
+	var nonAnswers = 0;
+	// var hideDescrip = function(){
+	// 	$(".description").hide ()
+	// };
 
+	console.log(questions[0]);
+
+	$("button").on('click', function (event){
+		// event.preventDefault();
+		$(".mainContent").show();
+	});
+
+});//closes document.ready function
